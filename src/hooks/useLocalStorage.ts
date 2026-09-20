ebe464de97cbea8973ef67875aa5dd6996a84257
Yoeun Seyha <seyha.yoeun@tsc.gov.kh>
2026-09-20 18:48:42 +0700

    implement custom hooks and testing

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T) {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const storedValue = window.localStorage.getItem(key);
+
+      return storedValue ? (JSON.parse(storedValue) as T) : initialValue;
+    } catch {
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
